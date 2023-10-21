0716090112564619c81560dc4b1bc491ffe5a4c4
galka <galka.new.york@gmail.com>
2023-10-21 12:40:04 -0400

    last steps and validation

--- src/main/java/com/cydeo/controller/TransactionController.java
+++ src/main/java/com/cydeo/controller/TransactionController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Controller
 public class TransactionController {
@@ -47,13 +49,35 @@ public class TransactionController {
         //I have UUID of accounts, but I need to provide Account object.
         //complete test conversion -> find accounts base on the ID and use as a param to complete make transfer method
         Account sender = accountService.retrieveByID(transaction.getSender());
-        Account receiver = accountService.retrieveByID(transaction.getSender());
+        Account receiver = accountService.retrieveByID(transaction.getReceiver());
 
 
         transactionService.makeTransfer(sender,receiver,transaction.getAmount(), new Date(),transaction.getMessage());
 
         return "redirect:/make-transfer";
     }
+    //TASK
+    //write a method that gets the account Id from index.html and print on the console
+    //(work on index.html and here)
+    //transaction/{id}
+    //return transaction/transactions page
+
+    @GetMapping("/transaction/{id}")
+    public String getTransactionList(@PathVariable("id") UUID id, Model model){
+        //print the id
+        System.out.println(id);
+
+        //get the list of transactions based on id and returns as a model attribute
+        //TASK -> complete the method (service and repository)
+        //findTransactionListById
+        //add model, return to UI, getting result, display
+
+        model.addAttribute("transactions", transactionService.findTransactionListById(id));
+
+        return "transaction/transactions";
+    }
+
+    //go to transactions.html --> put based on the transaction "No transactions yet" or  Transactions table
 
 }
 
